524822119b9e5bab84ea9eee46737b54e0c95a7c
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-04-15 21:58:23 +0100

    still setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <div>
+      <h1>hello</h1>
+    </div>
   )
 }
+
+export default App
